88887337e9a52932dd351cf7a61af5660d54fc54
Kyle <kylepcole@gmail.com>
2019-12-09 11:42:22 -0500

    buggy tests....

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+let game = document.getElementById('game');
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+	// let leftNumbers = dodger.style.left.replace("px", "");
+	let right = parseInt(dodger.style.left, 10);
+
+	// if ((right + dodger.clientWidth) < game.clientWidth) {
+	if ((right + dodger.clientWidth) < 400) { //game.clientWidth = 400....
+			dodger.style.left = `${right + 1}px`;
+	}
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
